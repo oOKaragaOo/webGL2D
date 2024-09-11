0ab29a01377031f53f2a327c73ed9a46cc2cdf2b
nattaphong KONGNANG OM <2210511101021@live4.utcc.ac.th>
2024-09-11 20:10:00 +0700

    have 9%

---
+++ js/data.js
@@ -0,0 +1,27 @@
+export const vertexData = [
+    0,   1, 0, // V1 Y position
+    0.5, 0.5, 0, // V2 position
+    -0.5, 0.5, 0, // V3 position
+
+    -0.3, 0.5, 0, // V1 Y position
+    -0.3,   0, 0, // V2 position
+    0.3,   0, 0, // V3 position
+
+    -0.3, 0.5, 0, // V1 Y position
+    0.3, 0.5, 0, // V2 position
+    0.3,   0, 0, // V3 position
+];
+
+export const colorData = [
+    0.278, 0.204, 0.482, // V1 Color
+    0.278, 0.204, 0.482, // V2 Color
+    0.278, 0.204, 0.482, // V3 Color
+
+    0, 1, 0, // V1 Color
+    0, 1, 0, // V2 Color
+    0, 1, 0, // V3 Color
+
+    0, 0, 1, // V1 Color
+    0, 0, 1, // V2 Color
+    0, 0, 1, // V3 Color
+];
--- refCollor.py
+++ refCollor.py
@@ -1,20 +1,22 @@
 import pyautogui
 from PIL import ImageGrab
 import time
-def get_mouse_color():
 
-    time.sleep(3)
-    # รับตำแหน่งของเมาส์
+def get_mouse_color():
+    time.sleep(2)
     x, y = pyautogui.position()
-    
-    # ถ่ายภาพหน้าจอบริเวณที่ตำแหน่งเมาส์
     screen = ImageGrab.grab(bbox=(x, y, x+1, y+1))
     
-    # ดึงค่าสีของพิกเซลที่ตำแหน่ง (0, 0) ในภาพ
     color = screen.getpixel((0, 0))
     
     return color
 
-# เรียกฟังก์ชัน
+def rgb_to_percentage(rgb_color):
+    return tuple(round(value / 255, 3) for value in rgb_color)
+
 color_at_mouse = get_mouse_color()
-print(f"RGB Color at mouse: {color_at_mouse}")
+
+color_percentage = rgb_to_percentage(color_at_mouse)
+
+print(f"RGB Color at mouse: {color_at_mouse} ")
+print(f"RGB Color in percentage (decimal): {color_percentage }")
